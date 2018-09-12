a2a22634110b0a51b01d8b921416d19c5c24a050
marong <youngwan@coupang.com>
2018-09-12 10:09:28 -0700

    fix db

--- src/main/java/com/calendar/reminder/Reminder.java
+++ src/main/java/com/calendar/reminder/Reminder.java
@@ -3,6 +3,7 @@ package com.calendar.reminder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.Accessors;
+import org.hibernate.annotations.Type;
 import org.joda.time.DateTime;
 
 import javax.persistence.*;
@@ -16,6 +17,8 @@ public class Reminder {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @Column(length = 1000)
     private String content;
+    @Type(type = "org.jadira.usertype.dateandtime.joda.PersistentDateTime")
     private DateTime date;
 }
--- src/main/java/com/calendar/reminder/ReminderApplication.java
+++ src/main/java/com/calendar/reminder/ReminderApplication.java
@@ -8,8 +8,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-//@EnableTransactionManagement
-//@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 public class ReminderApplication {
 
     public static void main(String[] args) {

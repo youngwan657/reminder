fcab3458f2f3f41dabe21953e915702264a1d35a
marong <youngwan@coupang.com>
2018-09-12 10:19:54 -0700

    change scehma

--- src/main/java/com/calendar/reminder/Reminder.java
+++ src/main/java/com/calendar/reminder/Reminder.java
@@ -2,6 +2,7 @@ package com.calendar.reminder;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 import lombok.experimental.Accessors;
 import org.hibernate.annotations.Type;
 import org.joda.time.DateTime;
@@ -11,13 +12,13 @@ import javax.persistence.*;
 @Entity
 @Getter
 @Setter
+@ToString
 @Accessors(chain = true)
 @Table
 public class Reminder {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    @Column(length = 1000)
     private String content;
     @Type(type = "org.jadira.usertype.dateandtime.joda.PersistentDateTime")
     private DateTime date;

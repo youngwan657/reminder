bcefd62f96b1a3620929a4cebd30e3b050cefcde
marong <youngwan@coupang.com>
2018-09-11 20:15:31 -0700

    init

---
+++ src/main/java/com/calendar/reminder/ReminderApplication.java
@@ -0,0 +1,12 @@
+package com.calendar.reminder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReminderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReminderApplication.class, args);
+	}
+}
